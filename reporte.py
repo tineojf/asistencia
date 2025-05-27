c35ac90e9bf144bef98dc783d0e8a297052a05e8
tineojf <92743762+tineodev@users.noreply.github.com>
2025-05-27 16:20:56 -0500

    refactor: streamline date handling and remove unused functions

--- reporte.py
+++ reporte.py
@@ -1,9 +1,9 @@
 import csv
-from datetime import datetime, date, timedelta
-import calendar
+import datetime
 
 archivo_entrada = "asistencia.csv"
 archivo_salida = "reporte_diario.csv"
+archivo_intervalo = "intervalo.csv"
 horarios = {
     "Elizabeth 1": ("08:00", "17:00"),
     "Orlando 3": ("08:00", "1:00"),
@@ -45,95 +45,51 @@ def crear_obj_trabajadores(tupla):
     return {item: {} for item in tupla}
 
 
+# Funcion - retorna lista con dias trabajados
+def dias_trabajables_desde_intervalo_csv():
+    with open(archivo_intervalo, mode="r", encoding="utf-8") as archivo:
+        lector = csv.reader(archivo)
+        next(lector)
+        fila = next(lector)
+        fecha_inicio = datetime.datetime.strptime(fila[0], "%d/%m/%Y").date()
+
+    fecha_fin = datetime.date.today() - datetime.timedelta(days=1)  # Ayer
+
+    dias_laborables = []
+    fecha_actual = fecha_inicio
+
+    while fecha_actual <= fecha_fin:
+        if fecha_actual.weekday() <= 5:  # lunes a sábado
+            dias_laborables.append(fecha_actual.strftime("%d/%m/%Y"))
+        fecha_actual += datetime.timedelta(days=1)
+
+    return dias_laborables
+
+
 # Main
 trabajadores = obtener_trabajadores_con_exclusion()
 obj_trabajadores = crear_obj_trabajadores(trabajadores)
-print(obj_trabajadores)
+# print(obj_trabajadores)
+# print(dias_trabajables_desde_intervalo_csv())
 
 
-def obtener_mes_actual():
-    fecha_actual = datetime.now()
-    return fecha_actual.month
-
-
-def obtener_mes_actual_formato():
-    fecha_actual = datetime.now()
-    return fecha_actual.strftime("%m")
-
-
-def obtener_dias_laborables(quincena):
-    mes = obtener_mes_actual()
-    anio = datetime.now().year
-
-    if quincena == "primera":
-        inicio = date(anio, mes, 1)
-        fin = date(anio, mes, 15)
-    elif quincena == "segunda":
-        ultimo_dia = calendar.monthrange(anio, mes)[1]
-        inicio = date(anio, mes, 16)
-        fin = date(anio, mes, ultimo_dia)
-    else:
-        raise ValueError("La quincena debe ser 'primera' o 'segunda'.")
-
-    dias_laborables = []
-    dia = inicio
-    while dia <= fin:
-        # Lunes-0 a sabado-5
-        if dia.weekday() < 6:
-            dias_laborables.append(dia.strftime("%d/%m/%Y"))
-        dia += timedelta(days=1)
-
-    return dias_laborables
-
-
-def obtener_dias_laborables_desde_dia(dia_inicio):
-    mes = obtener_mes_actual()
-    anio = datetime.now().year
-    hoy = datetime.now().date()
-
-    if dia_inicio > hoy.day:
-        raise ValueError(
-            f"El día de inicio ({dia_inicio}) no puede ser mayor que hoy ({hoy.day})."
-        )
-
-    if not (1 <= dia_inicio <= calendar.monthrange(anio, mes)[1]):
-        raise ValueError(
-            f"El día debe estar entre 1 y {calendar.monthrange(anio, mes)[1]}."
-        )
-
-    inicio = date(anio, mes, dia_inicio)
-    fin = hoy
-
-    dias_laborables = []
-    dia = inicio
-    while dia <= fin:
-        if dia.weekday() < 6:  # Lunes a sábado
-            dias_laborables.append(dia.strftime("%d/%m/%Y"))
-        dia += timedelta(days=1)
-
-    return dias_laborables
-
-
-def obtener_total_dias_laborables(dias_laborables):
-    return len(dias_laborables)
-
-
-def cargar_registros_por_trabajador(archivo_csv, trabajadores):
-    obj_trabajadores = {trabajador: [] for trabajador in trabajadores}
-
-    with open(archivo_csv, newline="", encoding="utf-8") as archivo:
-        lector = csv.DictReader(archivo)
-
-        for fila in lector:
-            nombre_fila = fila["Nombre"].strip()
-
-            for trabajador in trabajadores:
-                if nombre_fila == trabajador:
-                    registro = {
-                        "Tiempo": fila["Tiempo"],
-                        "Estado": fila["Estado"],
-                    }
-                    obj_trabajadores[trabajador].append(registro)
-                    break
-
-    return obj_trabajadores
+# def cargar_registros_por_trabajador(archivo_csv, trabajadores):
+# obj_trabajadores = {trabajador: [] for trabajador in trabajadores}
+#
+# with open(archivo_csv, newline="", encoding="utf-8") as archivo:
+# lector = csv.DictReader(archivo)
+#
+# for fila in lector:
+# nombre_fila = fila["Nombre"].strip()
+#
+# for trabajador in trabajadores:
+# if nombre_fila == trabajador:
+# registro = {
+# "Tiempo": fila["Tiempo"],
+# "Estado": fila["Estado"],
+# }
+# obj_trabajadores[trabajador].append(registro)
+# break
+#
+# return obj_trabajadores
+#
